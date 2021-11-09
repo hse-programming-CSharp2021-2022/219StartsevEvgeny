a8081f2195874b1e079986df5261b2fb1055943d
startsev2000 (Evgeny Startsev) <ebstartsev@edu.hse.ru>
2021-11-09 18:10:20 +0300

    Pushed some folders

---
+++ Module2/CW_2/CW_2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CW_2
+{
+    class MyComplex
+    {
+        private double re { get; set; }
+        private double im { get; set; }
+        public MyComplex(double xre, double xim)
+        {
+            re = xre; im = xim;
+        }
+        public static MyComplex operator ++(MyComplex mc)
+        {
+            return new MyComplex(mc.re + 1, mc.im + 1);
+        }
+        public static MyComplex operator --(MyComplex mc)
+        {
+            return new MyComplex(mc.re - 1, mc.im - 1);
+        }
+        public double Mod() 
+        { 
+            return Math.Abs(re * re + im * im);
+        }
+        static public bool operator true(MyComplex f)
+        {
+            if (f.Mod() > 1.0) return true;
+            return false;
+        }
+        static public bool operator false(MyComplex f)
+        {
+            if (f.Mod() <= 1.0) return true;
+            return false;
+        }
+
+        public static MyComplex operator +(MyComplex m1, MyComplex m2)
+        {
+            return new MyComplex(m1.re + m2.re, m1.im + m2.im);
+        }
+
+        public static MyComplex operator -(MyComplex m1, MyComplex m2)
+        {
+            return new MyComplex(m1.re - m2.re, m1.im - m2.im);
+        }
+
+        public static MyComplex operator *(MyComplex m1, MyComplex m2)
+        {
+            return new MyComplex(m1.re * m2.re - m1.im * m2.im, m1.im * m2.re + m1.re * m2.im);
+        }
+
+        public static MyComplex operator /(MyComplex m1, MyComplex m2)
+        {
+            return new MyComplex((m1.re * m2.re + m1.im * m2.im) / (m2.re * m2.re + m2.im + m2.im), (m1.im * m2.re - m1.re * m2.im) 
+                / (m2.re * m2.re + m2.im + m2.im));
+        }
+
+        static void Main(string[] args)
+        {
+            MyComplex myComplex1 = new MyComplex(1, 2);
+            MyComplex myComplex2 = new MyComplex(1, 2);
+            Console.WriteLine((myComplex1 + myComplex2).re);
+            Console.WriteLine((myComplex1 - myComplex2).re);
+            Console.WriteLine((myComplex1 * myComplex2).re);
+            Console.WriteLine((myComplex1 / myComplex2).re);
+        }
+    }
+}
---
+++ Module2/HW_1/HW_1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace HW_1
+{
+    class Point
+    {
+        private double X { get; set; }
+        private double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point() : this (0,0) { }
+
+        public double Ro
+        {
+            get
+            {
+                return Math.Sqrt(Y * Y + X * X);
+            }
+        }
+
+        public double Fi
+        {
+            get
+            {
+                if (X > 0 && Y >= 0)
+                {
+                    return Math.Atan(Y / X);
+                }
+                else if (X > 0 &&  Y < 0)
+                {
+                    return Math.Atan(Y / X) + 2 * Math.PI;
+                }
+                else if (X < 0)
+                {
+                    return Math.Atan(Y / X) + Math.PI;
+                }
+                else if (X == 0 && Y > 0)
+                {
+                    return Math.PI / 2;
+                }
+                else if (X == 0 && Y < 0)
+                {
+                    return 3 * Math.PI / 2;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public String PointData
+        {
+            get
+            {
+                string maket = "X = {0:F2}; Y = {1:F2}; Ro = {2:F2}; Fi = {3:F2} ";
+                return string.Format(maket, X, Y, Ro, Fi);
+            }
+        }
+
+        public static void Main()
+        {
+            Point a, b, c;
+            a = new Point(3, 4);
+            Console.WriteLine(a.PointData);
+            b = new Point(0, 3);
+            Console.WriteLine(b.PointData);
+            c = new Point();
+            double x = 0, y = 0;
+            do
+            {
+                Console.Write("x = ");
+                double.TryParse(Console.ReadLine(), out x);
+                Console.Write("y = ");
+                double.TryParse(Console.ReadLine(), out y);
+                c.X = x; c.Y = y;
+                Point[] points = new Point[3];
+                points[0] = a;
+                points[1] = b;
+                points[2] = c;
+            } while (x != 0 | y != 0);
+        }
+    }
+}
